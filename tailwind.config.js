6549800a04f270ef8f4386c1767c2eacc0e0ed98
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-06-07 08:35:06 +0200

    Deploy with Github actions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      // sans: ["Staatliches", ...defaultTheme.fontFamily.sans],
-      // mono: ["Major Mono Display", ...defaultTheme.fontFamily.mono],
+      sans: ["Staatliches", ...defaultTheme.fontFamily.sans],
+      mono: ["Major Mono Display", ...defaultTheme.fontFamily.mono],
     },
     extend: {},
   },
