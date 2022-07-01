45c073dfe3fa59a9316b642fd6dca348b94fb3e6
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-07-01 13:00:26 +0200

    open hacker applications

--- src/App.js
+++ src/App.js
@@ -18,24 +18,40 @@ function App() {
                 <span className="text-xl">ETHBerlin, to the power of 3.</span>
               </div>
               <div>September 16-18, 2022</div>
-              <div>Factory G&ouml;rlitzer Park</div>
+              <div>Factory G&ouml;rlitzer Park, Berlin</div>
+              <div>&nbsp;</div>
+              <div>
+                {" "}read our{" "}
+                <a href="https://office.ethberl.in/pad/#/2/pad/view/YY5CtWD3j+15ro-1itD78Pz0v2wd-9vUurTXdUyH7mc/embed/" rel="noreferrer" target ="_blank" className="underline">
+                  hacker manifesto
+                </a>{" "}
+              </div>
+              <div>
+                {" "}apply to{" "}
+                <a href="https://office.ethberl.in/form/#/2/form/view/Eb09ZMX-zPLH4GFYn8vM3yoNbZoGPxrVW1dPxUy37sg/embed/"  rel="noreferrer" target ="_blank" className="underline">
+                  hack
+                </a>,{" "}
+                <a href="https://office.ethberl.in/form/#/2/form/view/mX4c4Fi7V4wNHZX32D20RUiXTRfWV+ogehxpQkcxxH8/embed/"  rel="noreferrer" target ="_blank" className="underline">
+                  volunteer
+                </a>,{" "}
+                <a href="https://office.ethberl.in/form/#/2/form/view/YcBSf40HIEMYb04C4-LpA2kh2+NzeXj0EBppBYwYVmo/embed/"  rel="noreferrer" target ="_blank" className="underline">
+                  mentor
+                </a>{" "}
+              </div>
+              <div>
+                {" "}join our{" "}
+                <a href="https://matrix.to/#/%23ethberlin:matrix.org" rel="noreferrer" target ="_blank" className="underline">
+                  matrix space
+                </a>{" "}or{" "}
+                <a href="mailto:henlo@ethberlin.ooo" rel="noreferrer" target ="_blank" className="underline">
+                  email us
+                </a>{" "}
+              </div>
             </div>
           </div>
         )}
         <footer className="">
           <div className="mb-2">Department of Decentralization</div>
-          <div>
-            {" "}
-            <a href="https://office.ethberl.in/pad/#/2/pad/view/YY5CtWD3j+15ro-1itD78Pz0v2wd-9vUurTXdUyH7mc/embed/" rel="noreferrer" target ="_blank" className="underline">
-              hacker manifesto
-            </a>{" "}|{" "}
-            <a href="https://matrix.to/#/%23ethberlin:matrix.org" rel="noreferrer" target ="_blank" className="underline">
-              matrix space
-            </a>{" "}|{" "}
-            <a href="mailto:henlo@ethberlin.ooo" rel="noreferrer" target ="_blank" className="underline">
-              email us
-            </a>{" "}
-          </div>
           <button
             onClick={() => setShowImpressum(!showImpressum)}
             className="text-sm text-gray-600"
