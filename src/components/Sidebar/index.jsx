9dd6d29e7cf8a8727181cdd5145c55a190331aa2
kuzdogan <kaanuzdogan@hotmail.com>
2022-07-21 12:15:33 +0200

    Init design with ASCII eth

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col justify-between my-8 mx-4 font-w95 text-lg">
-      <nav className="flex flex-col">
+    <div className="h-screen flex flex-col justify-between my-8 mx-4 font-w95 fixed w-52">
+      <div className="flex flex-col justify-center font-digi">
+        <div className="flex text-[24pt]">
+          <div>ETHB3rlin</div>
+        </div>
+        <div className="font-w95">to the power of 3</div>
+      </div>
+      <nav className="flex flex-col mt-16">
         <a className="my-2">(a)bout</a>
         <a className="my-2">(s)chedule</a>
         <a className="my-2">(p)hotos</a>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,108 @@
 import React from "react";
+import EthDiamond from "../components/EthDiamond";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex-1 flex  min-h-full  text-berlin-yellow ">
+    <div className="flex-1 flex min-h-full  text-berlin-yellow font-w95">
       <Sidebar />
-      <div className="flex flex-1 flex-col items-center justify-center font-digi blur-text text-center">
-        <div className="flex text-[96pt]">
-          <div>ETHB3rlin</div>
+      <div className="flex flex-1 ml-64 text-sm">
+        <div className="mt-64 bg-black w-full mr-8 p-4">
+          <h1 className="my-4 underline">(a)bout</h1>
+          <div className="text-gray-50 text-justify">
+            <p>
+              Before reading, please note: this event is fully booked. We are
+              sorry but we are unable to accomodate more people. We will,
+              however repeat the experience improve based on what we learn, so
+              see you next time.{" "}
+            </p>
+            <p>
+              A lot has been rumored over ETHBerlin- or maybe not, but that was
+              a nice start for this write-up.
+            </p>{" "}
+            <p>
+              {" "}
+              The fact of the matter is that we are indeed working towards
+              organizing a memorable event, and while we sort out piles of GDPR
+              and legal paperwork because after all, Berlin’s cool but it’s in
+              Germany, and Germany does love bureaucracy, we need to focus more
+              on our audience and inform accordingly.{" "}
+            </p>
+            <p>
+              So we assembled this fact sheet in an FAQ format, which we intend
+              to continue adding to as questions arise.
+            </p>
+            <p>
+              Before reading, please note: this event is fully booked. We are
+              sorry but we are unable to accomodate more people. We will,
+              however repeat the experience improve based on what we learn, so
+              see you next time.{" "}
+            </p>
+            <p>
+              A lot has been rumored over ETHBerlin- or maybe not, but that was
+              a nice start for this write-up.
+            </p>{" "}
+            <p>
+              {" "}
+              The fact of the matter is that we are indeed working towards
+              organizing a memorable event, and while we sort out piles of GDPR
+              and legal paperwork because after all, Berlin’s cool but it’s in
+              Germany, and Germany does love bureaucracy, we need to focus more
+              on our audience and inform accordingly.{" "}
+            </p>
+            <p>
+              So we assembled this fact sheet in an FAQ format, which we intend
+              to continue adding to as questions arise.
+            </p>
+            <p>
+              Before reading, please note: this event is fully booked. We are
+              sorry but we are unable to accomodate more people. We will,
+              however repeat the experience improve based on what we learn, so
+              see you next time.{" "}
+            </p>
+            <p>
+              A lot has been rumored over ETHBerlin- or maybe not, but that was
+              a nice start for this write-up.
+            </p>{" "}
+            <p>
+              {" "}
+              The fact of the matter is that we are indeed working towards
+              organizing a memorable event, and while we sort out piles of GDPR
+              and legal paperwork because after all, Berlin’s cool but it’s in
+              Germany, and Germany does love bureaucracy, we need to focus more
+              on our audience and inform accordingly.{" "}
+            </p>
+            <p>
+              So we assembled this fact sheet in an FAQ format, which we intend
+              to continue adding to as questions arise.
+            </p>
+            <p>
+              Before reading, please note: this event is fully booked. We are
+              sorry but we are unable to accomodate more people. We will,
+              however repeat the experience improve based on what we learn, so
+              see you next time.{" "}
+            </p>
+            <p>
+              A lot has been rumored over ETHBerlin- or maybe not, but that was
+              a nice start for this write-up.
+            </p>{" "}
+            <p>
+              {" "}
+              The fact of the matter is that we are indeed working towards
+              organizing a memorable event, and while we sort out piles of GDPR
+              and legal paperwork because after all, Berlin’s cool but it’s in
+              Germany, and Germany does love bureaucracy, we need to focus more
+              on our audience and inform accordingly.{" "}
+            </p>
+            <p>
+              So we assembled this fact sheet in an FAQ format, which we intend
+              to continue adding to as questions arise.
+            </p>
+          </div>
         </div>
-        <div className="font-w95">to the power of 3</div>
+      </div>
+      <div className="mr-4">
+        <EthDiamond />
       </div>
     </div>
   );
