a715d419a4df508885f0eb7fdf1d955f842114f5
kuzdogan <kaanuzdogan@hotmail.com>
2022-08-01 13:00:51 +0300

    Banknotes

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -3,21 +3,34 @@ import React from "react";
 import dune from "../assets/sponsors/dune.svg";
 import manta from "../assets/sponsors/manta.png";
 import walletconnect from "../assets/sponsors/walletconnect.svg";
-
-const SponsorContainer = ({ children, tier, tierName, description }) => {
+import pengo from "../assets/sponsors/pengo.png";
+import dinar from "../assets/sponsors/dinar.png";
+import drachma from "../assets/sponsors/drachma.png";
+import papiermark from "../assets/sponsors/papiermark.png";
+import zimbabwe from "../assets/sponsors/zimbabwe.png";
+import bolivar from "../assets/sponsors/bolivar.png";
+const SponsorContainer = ({ children, tier, tierName, description, image }) => {
   return (
     <div
-      className="my-8 px-16 py-16"
+      className="my-8 px-16 py-16  overflow-clip"
       style={{ backgroundColor: "rgba(255,255,255, 0.05)" }}
     >
-      <div className="flex flex-col">
-        <div className="">
-          <div className="text-[32pt]">{tier}</div>
-          <div className="text-berlin-yellow text-2xl">{tierName}</div>
+      <div className="flex flex-col ">
+        <div className="flex flex-row">
+          <div className="flex-1 ">
+            <div className="text-[32pt]">{tier}</div>
+            <div className="text-berlin-yellow text-2xl">{tierName}</div>
+            <div className="mt-4">{description}</div>
+          </div>
+          <div className="">
+            <img
+              src={image}
+              className="rotate-12 translate-x-32 translate-y-8 h-48"
+            />
+          </div>
         </div>
-        <div className="mt-4">{description}</div>
+        <div className="flex justify-center items-center mt-24">{children}</div>
       </div>
-      <div className="flex justify-center items-center mt-16">{children}</div>
     </div>
   );
 };
@@ -40,6 +53,7 @@ const Sponsors = () => (
         tierName="Hungarian Pengő"
         tier="TIER 0"
         description="The Hungarian Pengő lost value dramitcally by 40 quadrillion percent after the Second World War (1946), suffering the highest rate of hyperinflation ever recorded."
+        image={pengo}
       >
         <a href="example.com">
           <img src={dune} className="h-16 mx-8 grayscale hover:grayscale-0" />
@@ -56,19 +70,35 @@ const Sponsors = () => (
           />
         </a>
       </SponsorContainer>
-      <SponsorContainer tierName="Zimbabwe Dollar" tier="TIER 1">
+      <SponsorContainer
+        tierName="Zimbabwe Dollar"
+        tier="TIER 1"
+        image={zimbabwe}
+      >
         TBD
       </SponsorContainer>
-      <SponsorContainer tierName="Yugoslav Dinar" tier="TIER 2">
+      <SponsorContainer tierName="Yugoslav Dinar" tier="TIER 2" image={dinar}>
         TBD
       </SponsorContainer>
-      <SponsorContainer tierName="Venezuelan Bolívar" tier="TIER 3">
+      <SponsorContainer
+        tierName="Venezuelan Bolívar"
+        tier="TIER 3"
+        image={bolivar}
+      >
         TBD
       </SponsorContainer>
-      <SponsorContainer tierName="Venezuelan Bolívar" tier="Community TIER">
+      <SponsorContainer
+        tierName="German Papiermark"
+        tier="Community TIER"
+        image={papiermark}
+      >
         TBD
       </SponsorContainer>
-      <SponsorContainer tierName="Greek Drachma" tier="CineBar TIER">
+      <SponsorContainer
+        tierName="Greek Drachma"
+        tier="CineBar TIER"
+        image={drachma}
+      >
         TBD
       </SponsorContainer>
     </div>
