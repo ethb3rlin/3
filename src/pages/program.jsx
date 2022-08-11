7be4a4219efc02929bd5af88685fd73cf5a1d85e
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2022-08-11 14:48:39 +0200

    Update title color

--- src/pages/program.jsx
+++ src/pages/program.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Program = () => (
   <Layout>
-    <h1 className="my-4 underline">(p)rogram</h1>
+    <h1 className="my-4 underline text-secondary">(p)rogram</h1>
     <div className="text-gray-50 text-justify">
       <h2 className="text-xl font-bold">Friday, September 16</h2>
       <p className="mt-4">
