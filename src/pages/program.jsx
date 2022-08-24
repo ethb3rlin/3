e133fadde5e725d8e3f6bfa2946fc0b5abd8abed
kuzdogan <kaanuzdogan@hotmail.com>
2022-08-24 15:33:47 +0200

    Add program TBD

--- src/pages/program.jsx
+++ src/pages/program.jsx
@@ -7,46 +7,129 @@ const Program = () => (
     <div className="text-gray-50 text-justify">
       <h2 className="text-xl font-bold">Friday, September 16</h2>
       <p className="mt-4">
-        Welcome to ETHBerlin³! If you have the chance, please claim your
-        badge early during the day outside the venue to avoid long queues
-        in the evening.
+        Welcome to ETHBerlin³! If you have the chance, please claim your badge
+        early during the day outside the venue to avoid long queues in the
+        evening.
+      </p>
+      <p>
+        Please note that the program below is subject to change and is just a
+        rough outline. Official program will be announced soon.
       </p>
       <p className="mt-4">
         <ul>
-          <li><span className="fake-bold">12:00</span> Pre-registration opens (Creator's Lab, Courtyard 3)</li>
-          <li><span className="fake-bold">16:00</span> Registration and doors open (Main entrance, Courtyard 1)</li>
-          <li><span className="fake-bold">17:00</span> Keynote 1 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">17:30</span> Keynote 2 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">18:00</span> Opening Ceremony (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">19:00</span> Team Matching (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">19:00</span> Hacking Starts (Community Spaces, Meeting Rooms)</li>
-          <li><span className="fake-bold">19:00</span> Dinner (Restaurant, Ground Floor)</li>
-          <li><span className="fake-bold">20:00</span> DJ/Music (Courtyard 1, Ground Floor)</li>
-          <li><span className="fake-bold">20:00</span> Technical Workshop 1 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">20:00</span> Technical Workshop 2 (Creator's Lab, Ground Floor)</li>
-          <li><span className="fake-bold">20:45</span> Technical Workshop 3 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">20:45</span> Technical Workshop 4 (Creator's Lab, Ground Floor)</li>
-          <li><span className="fake-bold">21:30</span> Technical Workshop 5 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">21:30</span> Technical Workshop 6 (Creator's Lab, Ground Floor)</li>
-          <li><span className="fake-bold">22:15</span> Technical Workshop 7 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">22:15</span> Technical Workshop 8 (Creator's Lab, Ground Floor)</li>
-          <li><span className="fake-bold">24:00</span> Midnight Snack (Community Spaces)</li>
+          <li>
+            <span className="fake-bold">12:00</span> Pre-registration opens
+            (Creator's Lab, Courtyard 3)
+          </li>
+          <li>
+            <span className="fake-bold">16:00</span> Registration and doors open
+            (Main entrance, Courtyard 1)
+          </li>
+          <li>
+            <span className="fake-bold">17:00</span> Keynote 1 (Lexis, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">17:30</span> Keynote 2 (Lexis, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">18:00</span> Opening Ceremony (Lexis,
+            Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">19:00</span> Team Matching (Lexis,
+            Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">19:00</span> Hacking Starts (Community
+            Spaces, Meeting Rooms)
+          </li>
+          <li>
+            <span className="fake-bold">19:00</span> Dinner (Restaurant, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">20:00</span> DJ/Music (Courtyard 1,
+            Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">20:00</span> Technical Workshop 1
+            (Lexis, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">20:00</span> Technical Workshop 2
+            (Creator's Lab, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">20:45</span> Technical Workshop 3
+            (Lexis, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">20:45</span> Technical Workshop 4
+            (Creator's Lab, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">21:30</span> Technical Workshop 5
+            (Lexis, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">21:30</span> Technical Workshop 6
+            (Creator's Lab, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">22:15</span> Technical Workshop 7
+            (Lexis, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">22:15</span> Technical Workshop 8
+            (Creator's Lab, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">24:00</span> Midnight Snack (Community
+            Spaces)
+          </li>
         </ul>
       </p>
       <h2 className="text-xl font-bold">Saturday, September 17</h2>
+      <p className="mt-4">Happy hacking, no distraction!</p>
       <p className="mt-4">
-        Happy hacking, no distraction!
-      </p>
-      <p className="mt-4">
-        <li><span className="fake-bold">09:00</span> Breakfast (Restaurant, Ground Floor)</li>
-        <li><span className="fake-bold">13:00</span> Lunch (Restaurant, Ground Floor)</li>
-        <li><span className="fake-bold">18:00</span> Documentary 1 (Cinema, Top Floor)</li>
-        <li><span className="fake-bold">19:00</span> Dinner (Restaurant, Ground Floor)</li>
-        <li><span className="fake-bold">20:00</span> Documentary 2 (Cinema, Top Floor)</li>
-        <li><span className="fake-bold">20:00</span> DJ/Music (Courtyard 1, Ground Floor)</li>
-        <li><span className="fake-bold">20:00</span> Documentary 3 (Cinema, Top Floor)</li>
-        <li><span className="fake-bold">22:00</span> DJ/Music (Creator's Lab, Ground Floor)</li>
-        <li><span className="fake-bold">24:00</span> Midnight Snack (Community Spaces)</li>
+        <li>
+          <span className="fake-bold">09:00</span> Breakfast (Restaurant, Ground
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">13:00</span> Lunch (Restaurant, Ground
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">18:00</span> Documentary 1 (Cinema, Top
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">19:00</span> Dinner (Restaurant, Ground
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">20:00</span> Documentary 2 (Cinema, Top
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">20:00</span> DJ/Music (Courtyard 1, Ground
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">20:00</span> Documentary 3 (Cinema, Top
+          Floor)
+        </li>
+        <li>
+          <span className="fake-bold">22:00</span> DJ/Music (Creator's Lab,
+          Ground Floor)
+        </li>
+        <li>
+          <span className="fake-bold">24:00</span> Midnight Snack (Community
+          Spaces)
+        </li>
       </p>
       <h2 className="text-xl font-bold">Sunday, September 18</h2>
       <p className="mt-4">
@@ -54,17 +137,50 @@ const Program = () => (
       </p>
       <p className="mt-4">
         <ul>
-          <li><span className="fake-bold">09:00</span> Breakfast (Restaurant, Ground Floor)</li>
-          <li><span className="fake-bold">11:00</span> Hacker Submission Deadline (Internet)</li>
-          <li><span className="fake-bold">12:00</span> Judging starts (Community Spaces, Meeting Rooms)</li>
-          <li><span className="fake-bold">13:00</span> Lunch (Restaurant, Ground Floor)</li>
-          <li><span className="fake-bold">14:30</span> Keynote 3 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">15:00</span> Keynote 4 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">15:30</span> Keynote 5 (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">16:00</span> Closing Ceremony (Lexis, Ground Floor)</li>
-          <li><span className="fake-bold">17:00</span> Closing Drinks (Courtyard 1, Ground Floor)</li>
-          <li><span className="fake-bold">18:00</span> Doors close (Main entrance, Courtyard 1)</li>
-          <li><span className="fake-bold">21:00</span> Afterparty (To be announced...)</li>
+          <li>
+            <span className="fake-bold">09:00</span> Breakfast (Restaurant,
+            Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">11:00</span> Hacker Submission Deadline
+            (Internet)
+          </li>
+          <li>
+            <span className="fake-bold">12:00</span> Judging starts (Community
+            Spaces, Meeting Rooms)
+          </li>
+          <li>
+            <span className="fake-bold">13:00</span> Lunch (Restaurant, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">14:30</span> Keynote 3 (Lexis, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">15:00</span> Keynote 4 (Lexis, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">15:30</span> Keynote 5 (Lexis, Ground
+            Floor)
+          </li>
+          <li>
+            <span className="fake-bold">16:00</span> Closing Ceremony (Lexis,
+            Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">17:00</span> Closing Drinks (Courtyard
+            1, Ground Floor)
+          </li>
+          <li>
+            <span className="fake-bold">18:00</span> Doors close (Main entrance,
+            Courtyard 1)
+          </li>
+          <li>
+            <span className="fake-bold">21:00</span> Afterparty (To be
+            announced...)
+          </li>
         </ul>
       </p>
     </div>
