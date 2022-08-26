36d91dbba868de9378cd3abc337b51c0260a27a4
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2022-08-26 18:26:28 +0200

    Contributors layout, add mentors (#19)

---
+++ src/assets/people/mentors/index.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    name: "Alex Stokes",
+    organization: {
+      name: "Ethereum Foundation",
+      url: "https://ethereum.org",
+    },
+    image: require("./AlexStokes_EthereumFoundation.png"),
+  },
+  {
+    name: "Andrej Berlin",
+    organization: {
+      name: "Deep Work",
+      url: "https://deepwork.studio/",
+    },
+    image: require("./AndrejBerlin_DeepWork.png"),
+  },
+  {
+    name: "Colin Schwarz",
+    organization: {
+      name: "ChainSafe",
+      url: "https:/chainsafe.io",
+    },
+    image: require("./ColinSchwarz_ChainSafe.jpg"),
+  },
+  {
+    name: "Dan",
+    organization: {
+      name: "Rysk Finance",
+      url: "https://www.rysk.finance/",
+    },
+    image: require("./dan_ryskfinance.jpeg"),
+  },
+  {
+    name: "Federico Kunze Küllmer",
+    organization: {
+      name: "Evmos",
+      url: "https://evmos.org/",
+    },
+    image: require("./Federico_Evmos.jpeg"),
+  },
+  {
+    name: "Greg The Greek",
+    organization: {
+      name: "ChainSafe",
+      url: "https:/chainsafe.io",
+    },
+    image: require("./GregTheGreek_ChainSafe.png"),
+  },
+  {
+    name: "Markus Waas",
+    organization: {
+      name: "soliditydeveloper.com",
+      url: "https://soliditydeveloper.com/",
+    },
+    image: require("./MarkusWaas_Soliditydeveloper.com.jpg"),
+  },
+  {
+    name: "Nuno Loureiro",
+    organization: {
+      name: "Ethereum Foundation",
+      url: "https://ethereum.org",
+    },
+    image: require("./NunoLoureiro_EthereumFoundation.jpg"),
+  },
+  {
+    name: "Owen Barnes",
+    organization: {
+      name: "owenbarnes.com",
+      url: "https://www.owenbarnes.com/",
+    },
+    image: require("./OwenBarnes_OwenBarnes.com.jpeg"),
+  },
+  {
+    name: "Pavel Kireyev",
+    organization: {
+      name: "INSEAD",
+      url: "https://www.insead.edu/",
+    },
+    image: require("./PavelKireyev_INSEAD.jpg"),
+  },
+  {
+    name: "Pranav Garg",
+    organization: {
+      name: "Router Protocol",
+      url: "https://www.routerprotocol.com/",
+    },
+    image: require("./PranavGarg_RouterProtocol.png"),
+  },
+  {
+    name: "Terence",
+    organization: {
+      name: "Prysmatic Labs",
+      url: "https://prysmaticlabs.com/",
+    },
+    image: require("./Terence_Prysm.png"),
+  },
+  {
+    name: "Theo Beutel",
+    organization: {
+      name: "VitaDAO",
+      url: "https://www.vitadao.com/",
+    },
+    image: require("./TheoBeutel_VitaDAO.jpg"),
+  },
+  {
+    name: "Tobias Schubotz",
+    organization: {
+      name: "Safe",
+      url: "https://gnosis-safe.io/",
+    },
+    image: require("./TobiasSchubotz_Safe.png"),
+  },
+  {
+    name: "Uladzislau Radkevich",
+    organization: {
+      name: "zkDAO",
+      url: "https://zkdao.io/",
+    },
+    image: require("./UladzislauRadkevich_zkDAO.jpg"),
+  },
+  {
+    name: "Willian Mitsuda",
+    organization: {
+      name: "Otterscan",
+      url: "https://github.com/wmitsuda/otterscan",
+    },
+    image: require("./WillianMitsuda_Otterscan.jpg"),
+  },
+  {
+    name: "Will Scott",
+    organization: {
+      name: "Protocol Labs",
+      url: "https://protocol.ai/",
+    },
+    image: require("./willscott_ProtocolLabs.png"),
+  },
+  {
+    name: "Yan Minagawa",
+    organization: {
+      name: "Datanauten",
+      url: "https://datanauten.de/",
+    },
+    image: require("./YanMinagawa_Datanauten.png"),
+  },
+  {
+    name: "Zannis",
+    organization: {
+      name: "Aave",
+      url: "https://aave.com/",
+    },
+    image: require("./Zannis_Aave.jpeg"),
+  },
+  {
+    name: "Zhehao",
+    organization: {
+      name: "0x Labs",
+      url: "https://www.0x.org/",
+    },
+    image: require("./Zhehao_0xlab.jpeg"),
+  },
+];
