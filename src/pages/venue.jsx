2e7a6c8e24dd914420de3a487eb590de0c7c9447
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-09-10 23:51:39 +0200

    Add unavailable 4th floor

--- src/pages/venue.jsx
+++ src/pages/venue.jsx
@@ -4,7 +4,7 @@ import groundFloor from "../images/groundFloor.png";
 import firstFloor from "../images/firstFloor.png";
 import secondFloor from "../images/secondFloor.png";
 import thirdFloor from "../images/thirdFloor.png";
-// import fourthFloor from "../images/fourthFloor.png";
+import fourthFloor from "../images/fourthFloor.png";
 import fifthFloor from "../images/fifthFloor.png";
 
 const Venue = () => {
@@ -13,7 +13,7 @@ const Venue = () => {
       <h1 className="my-4 underline text-secondary">(v)enue</h1>
       <div className="decorate-links flex flex-col items-center text-berlin-yellow mt-16">
         <div className="my-4 italic text-gray-300">
-          Gray marked areas and the 4th floor are not available for ETHBerlin
+          Gray marked areas and the 4th floor are not available for ETHBerlin³
         </div>
         <h2 className="text-xl lg:text-3xl  mb-4">Ground Floor / Floor 0</h2>
         <div className="relative mb-16 lg:mx-16">
@@ -30,6 +30,13 @@ const Venue = () => {
         <h2 className="text-xl lg:text-3xl  mb-4"> Third Floor / Floor 3 </h2>
         <div className="relative mb-16 lg:mx-16">
           <img src={thirdFloor} alt="third floor"></img>
+        </div>
+        <h2 className="text-xl lg:text-3xl  mb-4"> Fourth Floor / Floor 4 </h2>
+        <p className="text-gray-300">
+          The 4th floor is unavailable for ETHBerlin³{" "}
+        </p>
+        <div className="relative mb-16 lg:mx-16">
+          <img src={fourthFloor} alt="fourth floor"></img>
         </div>
         <h2 className="text-xl lg:text-3xl  mb-4"> Fifth Floor / Floor 5 </h2>
         <div className="relative mb-16 lg:mx-16">
