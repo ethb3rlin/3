71a7c850aa88005eccba1d6760d6219a756274cf
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-01-08 14:54:26 +0100

    Change .ooo to .org

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: "ETHBerlin4 2024",
-    siteUrl: `https://ethberlin.ooo`,
-    url: `https://ethberlin.ooo`,
-    description:
-      "ETHBerlin 3: three-day hackathon extravaganza, May 24-26, 2024",
+    siteUrl: `https://ethberlin.org`,
+    url: `https://ethberlin.org`,
+    description: "ETHBerlin 3: three-day hackathon extravaganza, May 24-26, 2024",
     twitterUsername: "@ETHBerlin",
     image: `/ETHBerlin.gif`,
   },
