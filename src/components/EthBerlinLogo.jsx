4507aeec91d162224273352ece1fcf775073d4d4
0xkkonrad <konrad.e.urban@gmail.com>
2024-01-25 14:17:39 +0100

    removed zero

--- src/components/EthBerlinLogo.jsx
+++ src/components/EthBerlinLogo.jsx
@@ -7,7 +7,7 @@ const EthBerlinLogo = React.forwardRef((props, ref) => {
       className={`font-dotpassport tracking-widest	 text-black hover:no-underline ${props.className} text-right `}
       href="/"
     >
-      <div className={`${props.titleClassName}`}>ETHBerlin04</div>
+      <div className={`${props.titleClassName}`}>ETHBerlin4</div>
       <div className={`${props.subtitleClassName}`}>
       <span className={`font-ocra`}>  &lt;&lt;&lt;&lt;IDENTITY&lt;&lt;CRISIS&lt;&lt; </span>
       </div>
