4e943196c516343a7186afbba91e19e2079b1709
0xkkonrad <konrad.e.urban@gmail.com>
2024-01-25 14:19:34 +0100

    04 to 4

--- src/components/ETHBerlin.jsx
+++ src/components/ETHBerlin.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ETHBerlin = ({ className }) => (
   <span className={`font-ocra text-sm ${className} break-all`}>
-    ETHBerlin&lt;&lt;04&lt;&lt;&lt;&lt;Identity&lt;&lt;Crisis&lt;&lt;
+    ETHBerlin&lt;&lt;4&lt;&lt;&lt;&lt;Identity&lt;&lt;Crisis&lt;&lt;
   </span>
 );
 
