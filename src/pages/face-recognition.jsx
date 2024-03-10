5a9a819b70a2bec8db8e91e012962f3d91707de8
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-03-10 11:46:09 +0100

    Working form for face recognition

--- src/pages/face-recognition.jsx
+++ src/pages/face-recognition.jsx
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import React, { useState, useRef } from "react";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
+import { LuScanFace } from "react-icons/lu";
 
 const FaceRecognition = () => {
   const [src, setSrc] = useState(null);
@@ -33,12 +34,16 @@ const FaceRecognition = () => {
 
   const getCroppedImg = async (image, crop) => {
     const canvas = document.createElement("canvas");
+    console.log(image);
     const scaleX = image.naturalWidth / image.width;
+    console.log("scaleX", scaleX);
     const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
+    console.log("scaleY", scaleY);
+    canvas.width = crop.width * scaleX;
+    canvas.height = crop.height * scaleY;
     const ctx = canvas.getContext("2d");
 
+    console.log("crop", crop);
     ctx.drawImage(
       image,
       crop.x * scaleX,
@@ -47,8 +52,8 @@ const FaceRecognition = () => {
       crop.height * scaleY,
       0,
       0,
-      crop.width,
-      crop.height
+      crop.width * scaleX,
+      crop.height * scaleY
     );
 
     return new Promise((resolve, reject) => {
@@ -66,6 +71,17 @@ const FaceRecognition = () => {
     });
   };
 
+  const handleReset = () => {
+    setSrc(null);
+    setPreviewImage(null);
+    setResultImage(null);
+    setCrop({
+      aspect: 1,
+      unit: "px",
+      width: 50,
+      height: 50,
+    });
+  };
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!crop.width || !crop.height) return; // Ensure crop dimensions are valid
@@ -95,47 +111,106 @@ const FaceRecognition = () => {
 
   return (
     <Layout>
-      <div className="text-justify decorate-links textbox">
+      <div className="decorate-links textbox">
         <h1 className="my-4 underline font-ocra text-berlin-yellow">
           &lt;&lt;f&lt;ace recognition
         </h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="file"
-            name="file"
-            accept="image/*"
-            onChange={handleFileChange}
-            required
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {src && (
-          <ReactCrop
-            src={src}
-            crop={crop}
-            aspect={1}
-            onImageLoaded={onImageLoaded}
-            onComplete={handleOnCropComplete}
-            onChange={(newCrop) => setCrop(newCrop)}
+        <div className="grid grid-cols-2 gap-12">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center"
           >
-            <img
-              src={src}
-              alt="Source Image"
-              ref={setImageRef}
-              style={{ maxWidth: "100%" }}
+            <div className="flex justify-center my-4">
+              <button
+                type="button"
+                onClick={handleReset}
+                className="borde text-berlin-yellow brightness-75 hover:brightness-90  font-bold py-2 px-4 rounded cursor-pointer"
+              >
+                Reset
+              </button>
+              <button
+                className="bg-berlin-yellow font-bold py-2 px-4 rounded hover:brightness-105"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+
+            {/* Hidden file input */}
+            <input
+              type="file"
+              id="fileInput"
+              className="hidden"
+              name="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              required
             />
-          </ReactCrop>
-        )}
+            {/* Styled label as a button */}
+            {/* Show crop tool if image is selected */}
+            {!src ? (
+              <label
+                htmlFor="fileInput"
+                className="flex items-center justify-center text-center w-full h-full border-dashed border-2 rounded-md border-berlin-yellow bg-gray-200 bg-opacity-30 hover:bg-opacity-70 cursor-pointer"
+                style={{
+                  paddingTop: "100%", // This will make the height equal to 100% of the width
+                  position: "relative", // Position relative to enable absolute positioning of the content inside
+                }}
+              >
+                <span
+                  className="flex flex-col items-center justify-center text-center w-full h-full text-berlin-yellow"
+                  style={{
+                    position: "absolute", // Absolutely position the content to be centered
+                    top: 0,
+                    left: 0,
+                  }}
+                >
+                  <LuScanFace className="text-6xl " />
+                  Upload face
+                </span>
+              </label>
+            ) : (
+              <div className="overflow-hidden relative">
+                <ReactCrop
+                  src={src}
+                  crop={crop}
+                  aspect={1}
+                  onImageLoaded={onImageLoaded}
+                  onComplete={handleOnCropComplete}
+                  onChange={(newCrop) => setCrop(newCrop)}
+                >
+                  <img
+                    src={src}
+                    alt="Source Image"
+                    ref={setImageRef}
+                    style={{ maxWidth: "100%" }}
+                  />
+                </ReactCrop>
+              </div>
+            )}
+          </form>
+          <div>
+            <div className="h-full">
+              <h2 className="text-center">Result</h2>
+              <div className="flex items-center justify-center h-full">
+                {resultImage ? (
+                  <img
+                    src={resultImage}
+                    alt="Processed result"
+                    className="my-4"
+                  />
+                ) : (
+                  <div> The result will appear here </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
         {previewImage && (
           <div>
             <h2>Preview:</h2>
             <img src={previewImage} alt="Preview" className="my-4" />
-          </div>
-        )}
-        {resultImage && (
-          <div>
-            <h2>Result:</h2>
-            <img src={resultImage} alt="Processed result" className="my-4" />
           </div>
         )}
       </div>
