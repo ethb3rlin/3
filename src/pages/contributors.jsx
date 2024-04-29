ab2bd53d60062e55cf521acb03143998e272dd5d
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-04-29 18:50:27 +0200

    Update src/pages/contributors.jsx
    Co-authored-by: Afri <58883403+q9f@users.noreply.github.com>

--- src/pages/contributors.jsx
+++ src/pages/contributors.jsx
@@ -147,7 +147,7 @@ const Contributors = () => {
           `}
             onClick={handleTeam}
           >
-            {"<<<Team<"}
+            {"<<Team<<<<<<"}
           </button>
           <button
             className={`${
