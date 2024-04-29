27eb4b253b6e6e95131a04d352df1ee63c141f85
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-04-29 19:05:51 +0200

    Fix 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { RandomReveal } from "react-random-reveal";
+import Typewriter from "typewriter-effect";
 
 const NotFound = () => (
   <div className="flex flex-col justify-center items-center min-h-screen font-ocra text-black">
-    <div className={`flex blur-text m-0 text-[96pt]`}>
-      <div className=" layers" data-text={`404`}>
-        <RandomReveal isPlaying duration={1} characters="404" />
-      </div>
+    <div className={`glitch layers  flex m-0 text-[72pt]`}>
+      <Typewriter
+        options={{
+          cursor: null,
+          strings: ["<<<<404<<"],
+          autoStart: true,
+          loop: true,
+          delay: 30,
+          deleteSpeed: 30,
+          pauseFor: 10000,
+        }}
+      />
     </div>
-    <div className={`glitch layers font-w95`} data-text="to the power of 3">
+    <div className={`glitch layers`}>
       couldn't find the page you are looking for...
     </div>
   </div>
