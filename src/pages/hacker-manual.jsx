218342fee72d301af5d04a20dea8e778880de86e
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-05-13 20:15:32 +0200

    Typo, styling

--- src/pages/hacker-manual.jsx
+++ src/pages/hacker-manual.jsx
@@ -108,8 +108,7 @@ const Hack = () => {
             </li>
           </ul>
         </div>
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
         <div id="travel">
           <h2 className="text-2xl mt-8 font-bold">Before the Hackathon</h2>
           <h3 className="text-xl mt-6 mt-4">Travel and Accommodation</h3>
@@ -226,8 +225,7 @@ const Hack = () => {
             take a shower.
           </p>
         </div>
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
         <div id="find-a-team">
           <h3 className="text-xl mt-6 font-bold">Find a Team/Hacker/Idea</h3>
           <p>
@@ -244,31 +242,32 @@ const Hack = () => {
             ceremony.
           </p>
         </div>
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
         <div id="zupass">
           <h3 className="text-xl mt-6 font-bold">Generate your Zupass PCD</h3>
           <p>
-            This year, we have a PCD (proof-carrying data) integration with Zupass
-            for ETHBerlin04. You can prove your attendance to the hackathon by generating
-            a zero-knowledge proof through the Zupass web application.
+            This year, we have a PCD (proof-carrying data) integration with
+            Zupass for ETHBerlin04. You can prove your attendance to the
+            hackathon by generating a zero-knowledge proof through the Zupass
+            web application.
           </p>
           <p>
             Visit{" "}
             <a
               href="https://zupass.org"
               target="_blank"
-              rel="noreferrer noopener">
+              rel="noreferrer noopener"
+            >
               zupass.org
             </a>{" "}
-            and sing up (or log in) with the same email address you used to register
-            for the hackathon. You will find an active ETHBerlin04 subscription
-            in your account.
+            and sing up (or log in) with the same email address you used to
+            register for the hackathon. You will find an active ETHBerlin04
+            subscription in your account.
           </p>
           <p>
             You will require this PCD for the faucets (if needed) and the final
-            submission of your hackathon project as well as voting on other people's
-            projects after the hackathon concluded.
+            submission of your hackathon project as well as voting on other
+            people's projects after the hackathon concluded.
           </p>
         </div>
         <div id="connect">
@@ -287,18 +286,17 @@ const Hack = () => {
           </p>
           <p>There is no Discord or Telegram.</p>
         </div>
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
         <div id="food">
           <h3 className="text-xl mt-6 font-bold">Food and Drinks</h3>
           <p>
             As per usual, this one is on us! ETHBerlin will cover all meals and
             drinks for the entire hackathon! This includes breakfasts, lunches,
             dinners, and midnight snacks as well as various soft drinks, mate
-            (of course!), water, coffee and beer. In addition to <em>regular</em>{" "}
-            food, there will be vegan and vegetarian options provided. Just come with an open mind, a
-            knowledge-thirsty brain and enthusiasm. Meal and snack times will be
-            on the program.
+            (of course!), water, coffee and beer. In addition to{" "}
+            <em>regular</em> food, there will be vegan and vegetarian options
+            provided. Just come with an open mind, a knowledge-thirsty brain and
+            enthusiasm. Meal and snack times will be on the program.
           </p>
         </div>
         <div id="hack">
@@ -326,7 +324,9 @@ const Hack = () => {
               to the exclusion from the event.
             </li>
           </ul>
-          <h4 className="text-l mt-4 font-bold">Minimum Submission Requirements</h4>
+          <h4 className="text-l mt-4 font-bold">
+            Minimum Submission Requirements
+          </h4>
           <ul>
             <li>A link to the open-source code must be provided.</li>
             <li>A short presentation file must describe your project.</li>
@@ -338,13 +338,14 @@ const Hack = () => {
           </ul>
         </div>
         <div id="faucets">
-          <h3 className="text-xl mt-6 font-bold">Faucets for Sepolia and Holesky</h3>
+          <h3 className="text-xl mt-6 font-bold">
+            Faucets for Sepolia and Holesky
+          </h3>
           <p>
-            Dedicated testnet-token faucets are available for ETHBerlin04 hackers.
+            Dedicated testnet-token faucets are available for ETHBerlin04
+            hackers.
           </p>
-          <p>
-            You can receive 500 testnet Ether on Sepolia and Holesky:
-          </p>
+          <p>You can receive 500 testnet Ether on Sepolia and Holesky:</p>
           <ul>
             <li>
               Sepolia:{" "}
@@ -352,7 +353,9 @@ const Hack = () => {
                 href="https://sepolia.ethberlin.org"
                 target="_blank"
                 rel="noreferrer noopener"
-              >sepolia.ethberlin.org</a>
+              >
+                sepolia.ethberlin.org
+              </a>
             </li>
             <li>
               Holesky:{" "}
@@ -360,12 +363,14 @@ const Hack = () => {
                 href="https://holesky.ethberlin.org"
                 target="_blank"
                 rel="noreferrer noopener"
-              >holesky.ethberlin.org</a>
+              >
+                holesky.ethberlin.org
+              </a>
             </li>
           </ul>
           <p>
-            You will have to sign in with Zupass to authorize a faucet drop to any
-            of your addresses. Thank you, pk910 &lt;3
+            You will have to sign in with Zupass to authorize a faucet drop to
+            any of your addresses. Thank you, pk910 &lt;3
           </p>
         </div>
         <div id="prizes-bounties">
@@ -427,8 +432,8 @@ const Hack = () => {
           <h4 className="text-l mt-4 font-bold">Excellence Awards</h4>
           <div>
             Back by popular demand, please welcome the Excellence Awards. The
-            winners of each of these get <strong>5,000 DAI</strong> and these are to recognise
-            the following:
+            winners of each of these get <strong>5,000 DAI</strong> and these
+            are to recognise the following:
           </div>
           <ul className="mt-2">
             <li>
@@ -455,7 +460,8 @@ const Hack = () => {
             eligible for this award can either pick from our wishlist{" "}
             <i>(coming soon™)</i> of contributions, or can build an entirely new
             thing that they consider to be a positive impact for future versions
-            of ETHBerlin. The winner of this track will receive <strong>4,000 DAI</strong>.
+            of ETHBerlin. The winner of this track will receive{" "}
+            <strong>4,000 DAI</strong>.
           </div>
           <h4 className="text-l mt-4 font-bold">The Hacker's Choice Award</h4>
           <div className="mt-2">
@@ -466,8 +472,7 @@ const Hack = () => {
             <strong>7,000 DAI</strong>.
           </div>
         </div>
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
         <div id="mentors">
           <h3 className="text-xl mt-6 font-bold">Help from Mentors</h3>
           <b>TL;DR</b>
@@ -521,7 +526,7 @@ const Hack = () => {
             morning.
           </p>
           <p>
-            We encounrage hackers to help each other out and keep an eye on the
+            We encourage hackers to help each other out and keep an eye on the
             mentoring matrix channel. There might be some hackers with very
             specific expert knowledge and it would be great if this could be
             shared.
@@ -545,8 +550,7 @@ const Hack = () => {
             <h3 className="text-xl mt-6 font-bold">Voting</h3>
             <p>The voting process will be explained soon.</p>
           </div>
-          <div id="party">
-          </div>
+          <div id="party"></div>
         </div>
 
         <VenueMapModal
@@ -557,8 +561,7 @@ const Hack = () => {
           activeRoomClass={"yard1"}
         />
 
-        <div className="flex justify-center">
-        </div>
+        <div className="flex justify-center"></div>
       </div>
     </Layout>
   );
